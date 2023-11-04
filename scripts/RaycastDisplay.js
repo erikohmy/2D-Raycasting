dc20f79ea9715bf4b29900b52cb1784663abdb86
erikohmy <erik@ohmy.nu>
2023-11-04 19:39:42 +0100

    code to select hovered plane, for future buttons

--- scripts/RaycastDisplay.js
+++ scripts/RaycastDisplay.js
@@ -7,6 +7,20 @@ class RaycastDisplay {
     frames = 0;
     fps = 0;
 
+    mousePos = {
+        rx: 0,
+        ry: 0,
+        x: 0,
+        y: 0
+    };
+
+    selectedTarget = undefined;
+
+    selectNonsolid = false;
+    selectHovered = false;
+    drawSelected = false;
+    drawCursor = false;
+
     constructor(engine, width, height) {
         this.engine = engine;
         this.canvas = document.createElement("canvas");
@@ -31,6 +45,22 @@ class RaycastDisplay {
         this.canvas.addEventListener("click", event => {
             this.engine.canvas.focus();
         });
+
+        let handleMouse = (event) => {
+            let bounds = this.canvas.getBoundingClientRect();
+            [this.mousePos.rx, this.mousePos.ry] = [event.x - bounds.x, event.y - bounds.y];
+
+            let actual_canvas_width = this.canvas.clientWidth;
+            let xpercent = this.mousePos.rx / actual_canvas_width;
+            this.mousePos.x = xpercent * this.size.width;
+
+            let actual_canvas_height = this.canvas.clientHeight;
+            let ypercent = this.mousePos.ry / actual_canvas_height;
+            this.mousePos.y = ypercent * this.size.height;
+        };
+        document.addEventListener("mousemove", handleMouse);
+        document.addEventListener("mousedown", handleMouse);
+        document.addEventListener("mouseup", handleMouse);
 
         setInterval(() => {
             this.fps = this.frames;
@@ -64,16 +94,53 @@ class RaycastDisplay {
         this.ctx.drawImage(img,sx,sy,sw,sh,x,y,w,h);
     }
 
+    getMouseSlice(resolution) {
+        let slice_width = this.size.width / resolution;
+        let x = Math.min(Math.max(this.mousePos.x, 0), this.size.width);
+        return Math.floor(x / slice_width);
+    }
+
     render(slices) {
         this.clearScreen();
         let slice_width = this.size.width / slices.length;
         let slice_height = this.sliceheight;
 
-
         // fill in the floor
         this.setcolor("#444");
         this.fillRect(0,this.size.height/2,this.size.width,this.size.height/2);
 
+        let hovered_x = this.getMouseSlice(slices.length);
+
+        // preprocess
+        this.selectedTarget = undefined;
+        for(let i=0;i<slices.length;i++) {
+            let slice = slices[i].toReversed();
+            for(let j=0;j<slice.length;j++) {
+                let hit = slice[j];
+                let target = hit.target
+                let slice_apparent_height = slice_height * (this.size.height/hit.distance_n);
+                if (this.selectHovered && i == hovered_x) {
+                    if ( this.mousePos.y > this.size.height/2 - slice_apparent_height/2 && this.mousePos.y < this.size.height/2 + slice_apparent_height/2) {
+                        if (this.selectNonsolid || target.solid) {
+                            this.selectedTarget = hit.target;
+                        }
+                    }
+                }
+                hit.apparent_height = slice_apparent_height;
+            }
+        }
+
+        if (this.selectHovered) {
+            if (this.selectedTarget) {
+                this.engine.selection = {
+                    type: "plane",
+                    target: this.selectedTarget
+                }
+            } else {
+                this.engine.selection = undefined;
+            }
+        }
+        
         for(let i=0;i<slices.length;i++) {
             let slice = slices[i].toReversed();
             let slice_x = slice_width * i;
@@ -81,9 +148,10 @@ class RaycastDisplay {
             for(let j=0;j<slice.length;j++) {
                 let hit = slice[j];
                 let target = hit.target
+                let selected = this.engine.isSelected(target);
 
                 // scale the height of the slice to the distance from the origin, newtonian perspective
-                let slice_apparent_height = slice_height * (this.size.height/hit.distance_n);
+                let slice_apparent_height = hit.apparent_height;
                 let slice_y = (this.size.height/2) - (slice_apparent_height/2);
 
                 this.ctx.globalAlpha = target.opacity;
@@ -97,6 +165,19 @@ class RaycastDisplay {
                     this.setcolor(target.color);
                     this.fillRect(slice_x,slice_y,slice_width,slice_apparent_height);
                 }
+
+                if (this.drawSelected && selected) {
+                    this.ctx.globalAlpha = 0.3;
+                    this.setcolor("#fff");
+                    this.fillRect(slice_x,slice_y,slice_width,slice_apparent_height);
+
+                    if (this.drawCursor && i == hovered_x) {
+                        this.ctx.globalAlpha = 1;
+                        this.setcolor("#000");
+                        this.fillRect(slice_x, this.mousePos.y - slice_width/2, slice_width, slice_width);
+                    }
+                }
+
                 this.ctx.globalAlpha = 1;
             }
         }
